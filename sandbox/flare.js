2ba17f4ae606a9ee8984fffa2b867d2beef5d840
Justin Makeig <justin@Heidis-iMac.local>
2011-02-13 10:33:50 -0800

    Sandbox treemap implementation

---
+++ sandbox/flare.js
@@ -0,0 +1,20 @@
+Number.prototype.pad = function(length, char) {
+	var str = '' + this;
+	while (str.length < length) {
+		str = char + str;
+	}
+	return str;
+}
+
+var flare = {};
+flare.hosts = {};
+for(var i = 1; i < 50; i++) {
+	var forests = {}
+	var max = Math.floor(Math.random()*11) + 1;
+	for(var j = 0; j < max; j++) {
+		forests["forest" + j.pad(3, "0")] = Math.floor(Math.random()*800) + 1;
+	}
+	flare.hosts["host" + i.pad(3, "0")] = forests;
+}
+
+console.dir(flare);
